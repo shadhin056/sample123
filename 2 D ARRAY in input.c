9ac696c58db965cd26a9e7d57562e30255d87518
Moniruzzaman Shadhin <shadhinemail@gmail.com>
2018-04-07 13:06:06 +0600

    added

--- 2 D ARRAY in input.c
+++ 2 D ARRAY in input.c
@@ -2,8 +2,9 @@
 int main()
 {
     int a[100][100];
+    int a[100][100];
     scanf("%d",&a[2][32]);
-    printf("%d\n",a[2][32]);
+    scanf("%d",&a[2][32]);
     printf("%d\n",a[2][32]);
     printf("%d\n",a[2][32]);
     return 0;
